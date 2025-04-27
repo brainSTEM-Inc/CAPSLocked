842583b5c8c20d5c01b0c50c04398eaf6c872a24
pineapply <annawuzhou@gmail.com>
2025-04-27 19:18:55 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+
+@app.route('/submit_availability', methods=['POST'])
+def submit_availability():
+    data = request.get_json()
+
+    roomsToTimes = data.get('roomsToTimes', {})
+    allTimes = data.get('allTimes', [])
+
+    print("Rooms to Times:", roomsToTimes)
+    print("All Times:", allTimes)
+
+    # Now you have both structures ready to use!
+    return jsonify({"message": "Success!"}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
