e652c834fa7a68cf7f7ac4c8387c1d54b532e4c2
pineapply <annawuzhou@gmail.com>
2025-04-28 18:57:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -93,11 +93,22 @@ def submit_availability():
     
     #print(initRoomDistribution)
     print(rawRoomData)
-
+    
     # Send the data to the frontend
     return render_template(
             "index.html"
         )
-    
+
+@app.route('/process', methods=['POST'])
+def process():
+    data = request.get_json()
+    rooms_to_times = data['roomsToTimes']
+    all_times = data['allTimes']
+
+    print("Rooms to Times:", rooms_to_times)
+    print("All Times:", all_times)
+
+    return 'Data received successfully!'
+
 if __name__ == '__main__':
     app.run(debug=True)
