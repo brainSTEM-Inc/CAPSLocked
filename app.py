82ba19059d47c1a38ef9d992c3b0ebc4ca514d2d
pineapply <annawuzhou@gmail.com>
2025-04-28 20:07:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ def submit_availability():
     #allTimes = ["PD 2 Tuesday, December 19th", "PD 3, Tuesday, December 19th", "PD 4, Tuesday, December 19th", "PD 5, Tuesday, December 19th", "PD 6, Tuesday, December 19th", "PD 2, Thursday, December 21st", "PD 3, Thursday, December 21st", "PD 4, Thursday, December 21st", "PD 5, Thursday, December 21st", "PD 6, Thursday, December 21st"]
     #roomToTimes={element:allTimes for element in ["Room 195","Room 198","Room 199"]}
 
-    roomsToTimes = data.get('roomsToTimes')
+    roomToTimes = data.get('roomsToTimes')
     allTimes = data.get('allTimes')
     
     roomToTimes = dict(sorted(roomToTimes.items(), key=lambda item: len(item[1]),reverse=True))
