d54f7ea88b60094719f81519df83ec5fc3523009
pineapply <annawuzhou@gmail.com>
2025-06-09 20:41:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1291,9 +1291,11 @@ def parse_data():
     #print(juniorRawdataDict)
     #print(juniorDisplayAllTimesFromData)
 
+    return render_template('index.html')
+    '''
     connection = psycopg2.connect(DATABASE_URL)
     cursor = connection.cursor()
-    '''
+    
     for name, values in rawdataDict.items():
         availability = ", ".join(values[0]) if values[0] else ""  # ✅ Availability (comma-separated)
         project_topic = values[1] if values[1] else ""  # ✅ Project Topic
@@ -1318,7 +1320,7 @@ def parse_data():
     
     # ✅ Commit changes and close connection
     connection.commit()
-    '''
+    
     for name, values in juniorRawdataDict.items():
         preferred_topics = values[0] if values[0] else ""  # ✅ Preferred Topics (comma-separated)
         availability = ", ".join(values[1]) if values[1] else ""  # ✅ Availability (comma-separated)
@@ -1335,10 +1337,10 @@ def parse_data():
     connection.commit()
     cursor.close()
     connection.close()
-
+    
 
     print("✅ Profiles tables successfully updated!")
-    return render_template('index.html')
+    '''
 
 
 
